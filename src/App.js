0fce0af878f0b1d1e003b9799d92feee4f41f7be
ddengmin <ammin1127@naver.com>
2023-03-06 22:41:55 +0900

    [chore] DiaryList 일기리스트 - 더미데이터에 임시 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id: 1,
+    author: "이정환",
+    content: "하이1",
+    emotion: 5,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 2,
+    author: "이정환",
+    content: "하이2",
+    emotion: 5,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 3,
+    author: "이정환",
+    content: "하이3",
+    emotion: 5,
+    created_date: new Date().getTime()
+    //new Date() - 현재시간 받기 getTime() - 시간을 받아서 밀리세컨즈로 변경 가능함.
+  }
+];
+
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor/>
       {/* 자식 요소로 추가 */}
+      <DiaryList diaryList={dummyList} />
+      {/* 자식 요소로 추가 */}
     </div>
   );
-}
+};
 
 export default App;
