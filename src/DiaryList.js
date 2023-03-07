270bc96e91b029f3dac40f5ab1b328d15efe75e1
ddengmin <ammin1127@naver.com>
2023-03-07 10:09:15 +0900

    [chore] React 배열 사용하기#1

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,26 +1,22 @@
+import DiaryItem from "./DiaryItem";
+
 const DiaryList = ({ diaryList }) => {
-  console.log(diaryList)
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
-      {diaryList.map((it,idx) => (
-        <div key={it.id}>
-          <div>작성자 : {it.author}</div>
-          <div>일기 : {it.content}</div>
-          <div>감정 : {it.emotion}</div>
-          <div>작성 시간(ms) : {it.created_date}</div>
-        </div>
-      ))}
+        {diaryList.map((it) => (
+          <DiaryItem key={it.id} {...it} />
+          //it.author ~ created_date를 한번에 다 받음
+        ))}
       </div>
     </div>
   );
 };
 
 DiaryList.defaultProps = {
-  diaryList: [],
+  diaryList: []
 };
-
 
 export default DiaryList;
