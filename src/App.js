253e363ba3728eab72cb5f26e3bbe19cd73cc55f
ddengmin <ammin1127@naver.com>
2023-03-07 17:57:01 +0900

    [chore] React Lifecycle 제어하기 - UseEffect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+import LifeCycle from "./LifeCycle";
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -33,16 +34,12 @@ const App = () => {
       )
     );
   };
-  //setData를 통해 값을 전달, onEdit은 배열에서 수정해줘야 하기 때문에 
-  //map이라는 내장함수를 이용한건데, 수정이 완료된 배열을 setData로 전달해야겠죵
-  //수정대상이라면 newContent로 바뀌게 될거고, 그러지 않은 값은 원본값을 지키겠지.
-
 
   return (
     <div className="App">
+      <LifeCycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
-      {/* 객체로 전달하기 때문에 DiaryList.js에도 전달해주자  */}
     </div>
   );
 };
