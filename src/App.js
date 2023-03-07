0cccf561dd82be7020a411f29873f4bd69d28084
ddengmin <ammin1127@naver.com>
2023-03-07 15:22:50 +0900

    [chore] React에서 배열 사용#3 - 데이터 삭제하기

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const App = () => {
+function App() {
   const [data, setData] = useState([]);
-  //useState
-  //DiaryEditor 컴포넌트와 DiaryList 컴포넌트가 함께 사용할 일기 data를 State로 가지고 있다
-  //그리고 그 일기 데이터는 빈배열로 시작, 일기 변화 함수인 setData 
-  //그래서 그 Data를 전달하면 밑에 DiaryList도 랜더가 되고 추가/삭제/수정 가능
+
   const dataId = useRef(0);
-  //useRef
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -21,17 +17,26 @@ const App = () => {
       created_date,
       id: dataId.current
     };
+
     dataId.current += 1;
+
     setData([newItem, ...data]);
-    //새로 올라온 일기를 추가[newItem], 그 이후에 내려주기
+  };
+
+  const onDelete = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다.`)
+    const newDiaryList = data.filter(
+      (it) => it.id !== targetId
+    );
+    setData(newDiaryList);
   };
 
   return (
     <div className="App">
-      <DiaryEditor onCreate={onCreate} />      
-      <DiaryList diaryList={data} />
-
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} onDelete={onDelete} />
     </div>
   );
-};
+}
+
 export default App;
