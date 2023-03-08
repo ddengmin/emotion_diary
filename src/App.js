5b41fbdf9abc2c78646cff4d8276c9e1d936870e
ddengmin <ammin1127@naver.com>
2023-03-08 10:11:02 +0900

    [chore] React에서 API 호출하기(slice / map / 난수)

--- src/App.js
+++ src/App.js
@@ -11,8 +11,18 @@ const App = () => {
     const res = await fetch(
       "https://jsonplaceholder.typicode.com/comments"
     ).then((res) => res.json());
-  console.log(res);
 
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime() + 1,
+        id: dataId.current++
+      };
+    });
+
+    setData(initData);
   };
 
   useEffect(() => {
